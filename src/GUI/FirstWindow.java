1ad947ceabad251e2b57b77ca10357deee116640
mciekn <macni.macni@gmail.com>
2021-05-17 14:56:58 +0200

    Prototyp pierwszego okna GUI

---
+++ src/GUI/FirstWindow.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FirstWindow {
+    private JButton DIODYButton;
+    private JButton BRAMKAXORButton;
+    private JTextField textField1;
+    private JButton gENEButton;
+    private JPanel FirstWindowRoot;
+
+    public Container getFirstWindowRoot(){
+        return this.FirstWindowRoot;
+    }
+}
