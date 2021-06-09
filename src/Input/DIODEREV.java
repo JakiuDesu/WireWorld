4a07d1152135a4e38bd074d78a8eb78fb6806268
JakiuDesu <jakub.krypno@gmail.com>
2021-06-09 21:53:20 +0200

    Dodanie Try Catch dla diod

--- src/Input/DIODEREV.java
+++ src/Input/DIODEREV.java
@@ -26,17 +26,19 @@ public class DIODEREV extends Gates {
     }
     @Override
     public void fileToTable(Board b, int x, int y) {
-        b.setPixel(x, y+6, 3);
-        b.setPixel(x, y+7, 3);
-        b.setPixel(x+2, y+6, 3);
-        b.setPixel(x+2, y+7, 3);
-        for(int i = 2; i < 15; i++) {
-            if(i==6) {
-                continue;
-            } else {
-                b.setPixel(x+1, y+i, 3);
+        try {
+            b.setPixel(x, y + 6, 3);
+            b.setPixel(x, y + 7, 3);
+            b.setPixel(x + 2, y + 6, 3);
+            b.setPixel(x + 2, y + 7, 3);
+            for (int i = 2; i < 15; i++) {
+                if (i == 6) {
+                    continue;
+                } else {
+                    b.setPixel(x + 1, y + i, 3);
+                }
             }
-        }
+        } catch (ArrayIndexOutOfBoundsException ignored) {};
 /*
         b.setPixel(x+1, y, 2);
         b.setPixel(x+1, y+1, 1);
