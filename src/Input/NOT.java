7954a73b33f48624b0f4ae82a192aae6df90e5ba
JakiuDesu <jakub.krypno@gmail.com>
2021-06-09 22:52:09 +0200

    Dodanie standardowych pozycji oraz bloków try catch

--- src/Input/NOT.java
+++ src/Input/NOT.java
@@ -8,69 +8,87 @@ public class NOT extends Gates{
     }
     @Override
     public void fileToTable(Board b) {
-        for(int i=5; i<8; i++) {
-            b.setPixel(5, i, 3);
-        }
-        b.setPixel(6, 4, 3);
-        b.setPixel(6, 8, 3);
-        for(int i=0; i<4; i++) {
-            b.setPixel(7, i, 3);
-        }
-        for(int i=7; i<10; i++) {
-            b.setPixel(7, i, 3);
-        }
-        for(int i=12; i<20; i++) {
-            b.setPixel(7, i, 3);
-        }
-        b.setPixel(8, 5, 3);
-        b.setPixel(8, 8, 3);
-        b.setPixel(8, 11, 3);
-        b.setPixel(9, 4, 3);
-        b.setPixel(9, 6, 3);
-        b.setPixel(9, 7, 3);
-        b.setPixel(9, 9, 3);
-        b.setPixel(9, 10, 3);
-        b.setPixel(10, 5, 3);
+        fileToTable(b, 8, 0);
 
-        b.setPixel(7, 1, 2);
-        b.setPixel(7, 2, 1);
-        b.setPixel(5, 5, 2);
-        b.setPixel(5, 6, 1);
-        b.setPixel(9, 4, 1);
-        b.setPixel(10, 5, 2);
+        b.setPixel(8, 0, 2);
+        b.setPixel(8, 1, 1);
+        b.setPixel(6, 4, 2);
+        b.setPixel(6, 5, 1);
+        b.setPixel(10, 3, 1);
+        b.setPixel(11, 4, 2);
     }
     @Override
     public void fileToTable(Board b, int x , int y) {
-        for(int i=5; i<8; i++) {
-            b.setPixel(x, y+i, 3);
+        for (int i = 4; i < 7; i++) {
+            try {
+                b.setPixel(x-2, y + i, 3);
+            } catch (ArrayIndexOutOfBoundsException ignored) {}
         }
-        b.setPixel(x+1, y+4, 3);
-        b.setPixel(x+1, y+8, 3);
-        for(int i=0; i<4; i++) {
-            b.setPixel(x+2, y+i, 3);
+        try {
+            b.setPixel(x - 1, y + 3, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x - 1, y + 7, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        for (int i = 0; i < 3; i++) {
+            try {
+                b.setPixel(x, y + i, 3);
+            } catch (ArrayIndexOutOfBoundsException ignored) {}
         }
-        for(int i=7; i<10; i++) {
-            b.setPixel(x+2, y+i, 3);
+        for (int i = 6; i < 9; i++) {
+            try {
+                b.setPixel(x, y + i, 3);
+            } catch (ArrayIndexOutOfBoundsException ignored) {}
         }
-        for(int i=12; i<15; i++) {
-            b.setPixel(x+2, y+i, 3);
+        for (int i = 11; i < 14; i++) {
+            try {
+                b.setPixel(x, y + i, 3);
+            } catch (ArrayIndexOutOfBoundsException ignored) {}
         }
-        b.setPixel(x+3, y+5, 3);
-        b.setPixel(x+3, y+8, 3);
-        b.setPixel(x+3, y+11, 3);
-        b.setPixel(x+4, y+4, 3);
-        b.setPixel(x+4, y+6, 3);
-        b.setPixel(x+4, y+7, 3);
-        b.setPixel(x+4, y+9, 3);
-        b.setPixel(x+4, y+10, 3);
-        b.setPixel(x+5, y+5, 3);
-/*
-        b.setPixel(x+2, y+1, 2);
-        b.setPixel(x+2, y+2, 1);
-        b.setPixel(x, y+5, 2);
-        b.setPixel(x, y+6, 1);
-        b.setPixel(x+4, y+4, 1);
-        b.setPixel(x+5, y+5, 2);
-*/
+        try {
+            b.setPixel(x + 1, y + 4, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x + 1, y + 7, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x + 1, y + 10, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x + 2, y + 3, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x + 2, y + 5, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x + 2, y + 6, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x + 2, y + 8, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x + 2, y + 9, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x + 3, y + 4, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x, y, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x, y + 1, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x-2, y + 4, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x-2, y + 5, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x + 2, y + 3, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
+        try {
+            b.setPixel(x + 3, y + 4, 3);
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
     }
 }
